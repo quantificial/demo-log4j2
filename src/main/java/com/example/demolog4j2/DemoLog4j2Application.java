2873a93e020c398946f90b8d4e1db17eabe033bd
Johnson Fu <johnson.fu@chubb.com>
2019-01-22 13:50:55 +0800

    add core

---
+++ src/main/java/com/example/demolog4j2/DemoLog4j2Application.java
@@ -0,0 +1,28 @@
+package com.example.demolog4j2;
+
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import lombok.extern.java.Log;
+
+@Log
+@SpringBootApplication
+public class DemoLog4j2Application implements ApplicationRunner {
+
+	public static void main(String[] args) {
+		
+		log.info("start application");
+		
+		SpringApplication.run(DemoLog4j2Application.class, args);
+	}
+	
+	@Override
+    public void run(ApplicationArguments applicationArguments) throws Exception {
+
+		log.info("testing...");
+    }	
+
+}
+
